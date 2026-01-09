35354a07a45f37c416b03eb74e1d1594a5990b85
level237 <bramslevel129@gmail.com>
2026-01-09 12:56:45 +0100

    feat: Add NativeWind and Tailwind CSS setup to the project.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}", 
+    "./src/**/*.{js,jsx,ts,tsx}", // Si vous mettez vos écrans dans un dossier /src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
